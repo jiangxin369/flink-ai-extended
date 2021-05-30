3fc9d46a204277fcf16c40d1fc27fb1f2dd47418
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-05-30 22:27:28 +0800

    Make sure resources would be released even though scheduler crash

--- flink-ai-flow/lib/airflow/airflow/contrib/jobs/event_based_scheduler_job.py
+++ flink-ai-flow/lib/airflow/airflow/contrib/jobs/event_based_scheduler_job.py
@@ -627,18 +627,14 @@ class EventBasedSchedulerJob(BaseJob):
             self.scheduler.submit_sync_thread()
             self.scheduler.recover(self.last_scheduling_id)
             self.scheduler.schedule()
-
+        finally:
+            self.log.info("Exited execute loop")
             self._stop_listen_events()
             self.periodic_manager.shutdown()
             self.dag_trigger.end()
             self.task_event_manager.end()
             self.executor.end()
-            
             settings.Session.remove()  # type: ignore
-        except Exception as e:  # pylint: disable=broad-except
-            self.log.exception("Exception when executing scheduler, %s", e)
-        finally:
-            self.log.info("Exited execute loop")
 
     def _start_listen_events(self):
         watcher = SchedulerEventWatcher(self.mailbox)
